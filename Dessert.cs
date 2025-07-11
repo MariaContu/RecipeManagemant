fe10608d64216b8f96fbf469cae3ed3be9bb1f65
Maria Eduarda Contu <mariaescontu@gmail.com>
2025-07-11 14:26:40 -0300

    adjusting dessert class

--- Dessert.cs
+++ Dessert.cs
@@ -2,11 +2,11 @@ namespace RecipesManagement
 {
     public class Dessert : Recipe
     {
-        private bool IsGlutenFree;
-        private bool IsBaked;
+        private bool IsGlutenFree { get; set; }
+        private bool IsBaked { get; set; }
 
         public Dessert(bool IsGlutenFree, bool IsBaked, string name, string description, int timeInMinutes, double portions, bool isVegetarian, List<Ingredient> ingredients,
-                List<string> instructions) : base(name, description, timeInMinutes, portions, isVegetarian, List < Ingredient > ingredients, instructions)
+                List<string> instructions) : base(name, description, timeInMinutes, portions, isVegetarian, ingredients, instructions)
         {
             this.IsGlutenFree = IsGlutenFree;
             this.IsBaked = IsBaked;
@@ -17,7 +17,7 @@ namespace RecipesManagement
         {
             PrintRecipe();
             Console.WriteLine($"Is gluten free? {(IsGlutenFree ? "Yes" : "No")}");
-            Console.WriteLine($"Is baked?: {(IsBaked ? "Yes" : "No")}");
+            Console.WriteLine($"Is baked? {(IsBaked ? "Yes" : "No")}");
         }
     }
 }
