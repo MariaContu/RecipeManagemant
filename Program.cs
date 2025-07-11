e1d8556d7d2dc3e5d23762e43204002e35887f8f
Isabela Rodrigues de Souza <bela.rodjj@gmail.com>
2025-07-11 11:38:00 -0300

    Adding .idea in .gitignore and creation of class Menu

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("╔══════════════════╗");
+        Console.WriteLine("║   RICA PANCITA   ║");
+        Console.WriteLine("╚══════════════════╝");
+        //mostar menu principal (1- categorias, 2- soborô e 3- sair)
+    }
+}
